5b074bb3d930dc1c47ba5583ffe2f5149f048fa5
or-katz <42887499+or-katz@users.noreply.github.com>
2019-02-06 18:48:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ set $user=user
 
 touch console
 
-if ps -ef |grep "[t]mux new-session -d -s $USER $(pwd)/connectToConsoleSaveOutput.sh" > /dev/null
+if ps -ef |grep "[t]mux new-session -d -s $USER $(pwd)/connectToConsole.sh" > /dev/null
 then
-	kill -9 $(ps aux |grep "[t]mux new-session -d -s $USER $(pwd)/connectToConsoleSaveOutput.sh" |awk '{print $2}')
+	kill -9 $(ps aux |grep "[t]mux new-session -d -s $USER $(pwd)/connectToConsole.sh" |awk '{print $2}')
 fi
 
 
@@ -18,15 +18,15 @@ fi
 
 
 
-tmux new-session -d -s "$USER" "$(pwd)/connectToConsoleSaveOutput.sh $host $user |tee -i -a $(pwd)/console" &
+tmux new-session -d -s "$USER" "$(pwd)/connectToConsole.sh $host $user |tee -i -a $(pwd)/console" &
 tail -f -n 1 $(pwd)/console |while read log; do logger "console_log - $log"; done &
 
 while true >> /dev/null ; do
 	sleep 5
 	sed -i -n '$p' $(pwd)/console &
-	if ! ps -ef |grep "[t]mux new-session -d -s $USER $(pwd)/connectToConsoleSaveOutput.sh" > /dev/null
+	if ! ps -ef |grep "[t]mux new-session -d -s $USER $(pwd)/connectToConsole.sh" > /dev/null
 		then
-		tmux new-session -d -s "$USER" "$(pwd)/connectToConsoleSaveOutput.sh raspberrypi pi |tee -i -a $(pwd)/console" &
+		tmux new-session -d -s "$USER" "$(pwd)/connectToConsole.sh raspberrypi pi |tee -i -a $(pwd)/console" &
 		tail -f -n 1 $(pwd)/console |while read log; do logger "console_log - $log"; done &
 	fi
 		
